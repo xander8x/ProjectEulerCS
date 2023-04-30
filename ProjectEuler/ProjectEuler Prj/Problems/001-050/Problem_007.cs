45e6fa502429c7f151772dc50aeeb9954bb4aa1c
xander8x <xander8x@gmail.com>
2023-05-01 00:12:12 +0200

    [PE][Problem] Fix Problem_007

--- ProjectEuler/ProjectEuler Prj/Problems/001-050/Problem_007.cs
+++ ProjectEuler/ProjectEuler Prj/Problems/001-050/Problem_007.cs
@@ -9,7 +9,7 @@ namespace ProjectEuler.Problems
 {
     [ProblemAttributes("10001st prime",
 @"By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
-What is the 10001st prime number?", false, 7)]
+What is the 10001st prime number?", true, 7)]
 
     [ProblemSolutionInfo(19, 104743)]
     public class Problem007 : ProblemBase
