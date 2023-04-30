190b706aa1ef0d4f55d6fde14d602c776d66451c
xander8x <xander8x@gmail.com>
2023-04-30 16:05:12 +0200

    [PE][Problem] Added Problem_004

---
+++ ProjectEuler/ProjectEuler Prj/Problems/001-050/Problem_004.cs
@@ -0,0 +1,26 @@
+﻿using ProjectEuler.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems
+{
+    [ProblemAttributes("Largest palindrome product",
+"A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99." +
+"\r\nFind the largest palindrome made from the product of two 3-digit numbers.",
+        true,
+        4)]
+    [ProblemSolutionInfo(15, 906609)]
+    class Problem004 : ProblemBase
+    {
+        protected override object InternalExecute()
+        {
+            return ProjectEulerHelper.Range(900, 99)
+                .SelectMany(b => ProjectEulerHelper.Range(900, 99).Select(a => (a * b)))
+                .Where(x => x.IsPalindrome())
+                .Max();
+        }
+    }
+}
