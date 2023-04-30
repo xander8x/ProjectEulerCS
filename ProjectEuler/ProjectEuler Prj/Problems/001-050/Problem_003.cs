b3af30d7e6e22fd68591b30f3a108bdc7f974c3f
xander8x <xander8x@gmail.com>
2023-04-30 15:40:53 +0200

    [PE][Problem] Added Problem_003

---
+++ ProjectEuler/ProjectEuler Prj/Problems/001-050/Problem_003.cs
@@ -0,0 +1,24 @@
+﻿using ProjectEuler.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems
+{
+    [ProblemAttributes("Largest prime factor",
+        "The prime factors of 13195 are 5, 7, 13 and 29." +
+        "\r\n \r\nWhat is the largest prime factor of the number 600851475143 ?",
+        true,
+        3)
+    ]
+    [ProblemSolutionInfo(4, 6857)]
+    public class Problem003 : ProblemBase
+    {
+        protected override object InternalExecute()
+        {
+            return ProjectEulerHelper.LargestPrimeFactor(600851475143);
+        }
+    }
+}
