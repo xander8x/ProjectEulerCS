a1825f7e6cd65b98681e049454ba3e7b0ffa7bae
xander8x <xander8x@gmail.com>
2023-05-01 00:25:47 +0200

    [PE][Problem] Added Problem_010

---
+++ ProjectEuler/ProjectEuler Prj/Problems/001-050/Problem_010.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectEuler.Helper;
+
+namespace ProjectEuler.Problems
+{
+    [ProblemAttributes("Summation of primes",
+@"The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+Find the sum of all the primes below two million.",
+        false,
+        10)]
+    [ProblemSolutionInfo(62, 142913828922)]
+    public class Problem010 : ProblemBase
+    {
+        protected override object InternalExecute()
+        {
+            var upperLimit = 2000000;
+            return ProjectEulerHelper.GetPrimes(upperLimit).Aggregate(BigInteger.Zero, (x, y) => x + y);
+
+        }
+    }
+}
