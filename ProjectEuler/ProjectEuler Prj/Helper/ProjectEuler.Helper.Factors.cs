afdc9fee6476597b25d769b368f7f9177656dd24
xander8x <xander8x@gmail.com>
2023-05-01 01:19:11 +0200

    [PE][Helper][Factors] Added "GetNumberOfFactorsByPrimes" and "Factors" methods.

--- ProjectEuler/ProjectEuler Prj/Helper/ProjectEuler.Helper.Factors.cs
+++ ProjectEuler/ProjectEuler Prj/Helper/ProjectEuler.Helper.Factors.cs
@@ -38,5 +38,62 @@ namespace ProjectEuler.Helper
 
             return result;
         }
+
+
+        /// <summary>
+        /// Given a list of prime numbers, evaluate the number of factors of the input value.
+        /// </summary>
+        /// <param name="value">Value whose number of factors is computed.</param>
+        /// <param name="primelist">List of prime numbers used to evalute number of factors.</param>
+        /// <returns>Number of factors of <paramref name="value"/>.</returns>
+        public static BigInteger GetNumberOfFactorsByPrimes(BigInteger value, IEnumerable<BigInteger> primelist)
+        {
+            BigInteger nod = BigInteger.One;
+            BigInteger exponent = BigInteger.One;
+            BigInteger remain = value;
+
+
+            foreach (var prime in primelist)
+            {
+                if (prime * prime > value) return nod * 2;
+
+                exponent = BigInteger.One;
+                while (remain % prime == 0)
+                {
+                    exponent++;
+                    remain = remain / prime;
+                }
+                nod *= exponent;
+
+                if (remain == 1)
+                {
+                    return nod;
+                }
+            }
+
+            return nod;
+
+        }
+
+
+        /// <summary>
+        /// Returns all factors of input value.
+        /// </summary>
+        /// <param name="x">Value whose extract list of factors is coputed.</param>
+        /// <returns>List of factors belong to <paramref name="x"/>.</returns>
+        public static IEnumerable<BigInteger> Factors(this BigInteger x)
+        {
+            for (BigInteger i = 1; i * i <= x; i++)
+            {
+                if ((x % i) == 0)
+                {
+                    yield return i;
+                    if (i != (x / i))
+                    {
+                        yield return x / i;
+                    }
+                }
+            }
+        }
     }
 }
