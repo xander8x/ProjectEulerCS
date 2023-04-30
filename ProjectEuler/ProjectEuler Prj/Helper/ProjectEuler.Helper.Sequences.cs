cf1f14a43b943478579e25f1a14f281214413468
xander8x <xander8x@gmail.com>
2023-04-30 15:31:30 +0200

    [PE][Main] Added "Sequences" as helper and rename "Extension" into "Extentions"

---
+++ ProjectEuler/ProjectEuler Prj/Helper/ProjectEuler.Helper.Sequences.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Helper
+{
+    public static partial class ProjectEulerHelper
+    {
+        private static IEnumerable<BigInteger> FibonacciSequence(Func<BigInteger, bool> cancOnValue, Func<BigInteger, bool> cancOnResult)
+        {
+            BigInteger prev = 0;
+            BigInteger next = 1;
+            BigInteger sum = 1;
+            BigInteger iter = 1;
+
+            bool stop = false;
+            for (; !stop;)
+            {
+                if (cancOnResult != null && cancOnResult(iter))
+                {
+                    stop = true;
+                }
+                sum = prev + next;
+
+                if (cancOnValue != null && cancOnValue(sum))
+                {
+                    stop = true;
+                }
+                prev = next;
+                next = sum;
+                iter++;
+
+                yield return sum;
+            }
+        }
+
+        public static IEnumerable<BigInteger> GetFibonacciSequenceLimitedOnMaxValue(BigInteger limitOn)
+        {
+            Func<BigInteger, bool> _limit = new Func<BigInteger, bool>((v) => { return v > limitOn; });
+            return FibonacciSequence(_limit, null);
+        }
+
+        public static IEnumerable<BigInteger> GetFibonacciSequenceLimitedOnValue(Func<BigInteger, bool> limitOn)
+        {
+
+            return FibonacciSequence(limitOn, null);
+        }
+
+        public static IEnumerable<BigInteger> GetFibonacciSequenceLimitedOnMaxSize(BigInteger limitOn)
+        {
+            Func<BigInteger, bool> _limit = new Func<BigInteger, bool>((v) => { return v > limitOn; });
+            return FibonacciSequence(null, _limit);
+        }
+    }
+}
