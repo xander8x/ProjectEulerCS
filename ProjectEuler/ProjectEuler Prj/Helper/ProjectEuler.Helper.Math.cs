2fec8c02c9e878765297a7e1dd370d2785a62b38
xander8x <xander8x@gmail.com>
2023-05-01 14:56:32 +0200

    [PE][Helper] Added "Math" as helper.

---
+++ ProjectEuler/ProjectEuler Prj/Helper/ProjectEuler.Helper.Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Helper
+{
+    public static partial class ProjectEulerHelper
+    {
+        /// <summary>
+        /// Returns factorial of input value.
+        /// </summary>
+        /// <param name="value">Value whose factorial is computed.</param>
+        /// <returns>Factorial of <paramref name="value"/>.</returns>
+        public static BigInteger GetFactorial(BigInteger value)
+        {
+            BigInteger result = BigInteger.One;
+            if (value > 1)
+            {
+                result = value * GetFactorial(value - 1);
+            }
+
+            return result;
+
+        }
+    }
+}
