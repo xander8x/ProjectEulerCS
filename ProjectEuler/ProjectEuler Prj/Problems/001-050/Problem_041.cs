2d1c15e45572578a94411281ada6c788ae76bb21
xander8x <xander8x@gmail.com>
2026-09-10 23:53:30 +0200

    [PE][Problem] Added Problem_041.

---
+++ ProjectEuler/ProjectEuler Prj/Problems/001-050/Problem_041.cs
@@ -0,0 +1,29 @@
+﻿using ProjectEuler.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler.Problems._001_050
+{
+    [ProblemAttributes("Champernowne's Constant",
+        @"
+We shall say that an -digit number is pandigital if it makes use of all the digits  to  exactly once. For example, 2143 is a 4-digit pandigital and is also prime.
+
+What is the largest -digit pandigital prime that exists?",
+        true,
+        41)
+    ]
+    [ProblemSolutionInfo(73, 7652413)]
+    public class Problem041 : ProblemBase
+    {
+        protected override object InternalExecute()
+        {
+            return ProjectEulerHelper.GetAllPermutations(1234567)
+                                       .Where(x => x.IsEven == false)
+                                       .Where(x => x.IsPrime())
+                                       .OrderByDescending(x => x)
+                                       .FirstOrDefault();
+        }
+    }
+}
